f02913934369710ac4796f1e55300782b0795012
tomroquand-web <tom.roquand@asklocala.com>
2026-06-08 14:37:18 -0400

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -114,4 +114,5 @@ app.post("/slack/events", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("✅ Feedback bot (MOCK MODE) running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Feedback bot (MOCK MODE) running on port ${PORT}`));
